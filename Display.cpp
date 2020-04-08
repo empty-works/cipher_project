16e62ca03da8988d792c1c306424dc972d1c567f
empty-works <pama.mark@gmail.com>
2020-04-08 21:19:37 +0200

    Implemented some show code for Display.

---
+++ Display.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Display.h"
+
+Display::Display() {
+
+
+}
+
+Display::~Display() {
+
+
+}
+
+void Display::show() {
+
+	char selection{};
+
+	do {
+	
+
+	}
+	while(selection != 'q' && selection != 'Q');
+}
--- Display.h
+++ Display.h
@@ -1,1 +1,14 @@
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
 
+#include <iostream>
+
+class Display {
+
+public:
+	Display();
+	~Display();
+	void show();	
+};
+
+#endif
