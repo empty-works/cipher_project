5c65dc522d3e9dfd0814b31c32a881509390c0ad
empty-works <pama.mark@gmail.com>
2020-04-22 23:47:37 +0200

    Added cipher key again.

--- Sorter.h
+++ Sorter.h
@@ -8,15 +8,15 @@ class Sorter {
 private:
 	std::string user_input{};
 	size_t num_shifts{3};
-	std::string alpha_key{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-public:
 	Sorter();
 	~Sorter();
 	
 	void encrypt(const std::string user_input);
 	void decrypt(const std::string user_input);
 
-	void set_num_shifts(const size_t in_shifts);	
+	void set_num_shifts(const size_t in_shifts);
+protected:
+	std::string cipher_key{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
 };
 
 #endif
